b699e3def1a66ffe3afc4330aa2fcc16c131bb18
iamajeeth <rajeethkumarind01@gmail.com>
2021-10-07 21:18:40 +0530

    get github profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,9 @@ const Profile = require('../../models/Profile');
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 const { check, validationResult } = require('express-validator');
+const request = require('request');
+const config = require('config');
+
 //@GET api/profile/me
 //@desc get logged in user profile
 //@ access private
@@ -277,6 +280,33 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     profile.education.splice(removeEduIndex);
     await profile.save();
     res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route GET/api/profile/github/:username
+// @desc get repos of github
+// @access public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created.asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }, //user-agent allows server and networks to know the application ,os. in our case node.js is a server
+    };
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+      if (response.statusCode !== 200) {
+        res.status(404).json({ msg: 'No Github profile found' });
+      }
+      res.json(JSON.parse(body)); //body is usually a string type surrond with double quotes so we need to parse to JSON format
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
