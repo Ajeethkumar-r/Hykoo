ea51552ffb09b7476a309ab9906bf6e3ae318158
iamajeeth <rajeethkumarind01@gmail.com>
2021-10-08 16:24:12 +0530

    get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,4 +35,20 @@ router.post(
   }
 );
 
+// @route GET/api/posts
+// @desc get all posts
+// @access private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    if (!posts) {
+      return res.status(404).json({ msg: 'User not posted yet' });
+    }
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
