4293f5b197f29c86d8d7a3d71cdee741197481c1
iamajeeth <rajeethkumarind01@gmail.com>
2021-10-09 10:12:10 +0530

    unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -116,4 +116,30 @@ router.put('/like/:id', auth, async (req, res) => {
   }
 });
 
+// @route PUT/api/posts/unlike/:id
+// @desc unlike a post
+// @access private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //check the post has to like
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post has not liked' });
+    }
+    const removeLikeIndex = post.likes.map((like) =>
+      like.user.toString().indexOf(req.user.id)
+    );
+    post.likes.splice(removeLikeIndex, 1);
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
