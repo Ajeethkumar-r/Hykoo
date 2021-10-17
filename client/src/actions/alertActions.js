794f963773d2ac4199db7e3c17eacf718bc6cff3
iamajeeth <rajeethkumarind01@gmail.com>
2021-10-17 17:53:53 +0530

    feat: Alert removed base on time

--- client/src/actions/alertActions.js
+++ client/src/actions/alertActions.js
@@ -1,11 +1,15 @@
 import uuid from 'uuid/v4';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => (dispatch) => {
-  const id = uuid();
+export const setAlert =
+  (msg, alertType, timeOut = 3000) =>
+  (dispatch) => {
+    const id = uuid();
 
-  dispatch({
-    type: SET_ALERT,
-    payload: { msg, alertType, id },
-  });
-};
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, alertType, id },
+    });
+
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeOut);
+  };
