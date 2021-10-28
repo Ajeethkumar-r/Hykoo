5ea4cf3005fa2b857fe6cb4c92266bd41ac42c14
iamajeeth <rajeethkumarind01@gmail.com>
2021-10-28 17:10:00 +0530

    feat: posts need to fix date

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,4 @@
 import api from '../utils/api';
-import { setAlert } from './alertActions';
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -8,13 +7,18 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from './types';
+import { setAlert } from './alertActions';
+import setAuthToken from '../utils/setAuthToken';
 
-// Load User
 export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
   try {
     const res = await api.get('/auth');
-
     dispatch({
       type: USER_LOADED,
       payload: res.data,
@@ -26,54 +30,67 @@ export const loadUser = () => async (dispatch) => {
   }
 };
 
-// Register User
-export const register = (formData) => async (dispatch) => {
-  try {
-    const res = await api.post('/users', formData);
+export const register =
+  ({ name, email, password }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const body = JSON.stringify({ name, email, password });
 
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data,
-    });
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
+    try {
+      const res = await api.post('/users', body, config);
 
-    if (errors) {
-      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+      dispatch(loadUser());
+    } catch (err) {
+      const errors = err.response.data.errors;
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+      dispatch({
+        type: REGISTER_FAIL,
+      });
     }
+  };
 
-    dispatch({
-      type: REGISTER_FAIL,
-    });
-  }
-};
-
-// Login User
 export const login = (email, password) => async (dispatch) => {
-  const body = { email, password };
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const body = JSON.stringify({ email, password });
 
   try {
-    const res = await api.post('/auth', body);
+    const res = await api.post('/auth', body, config);
 
     dispatch({
       type: LOGIN_SUCCESS,
       payload: res.data,
     });
-
     dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
-
     if (errors) {
       errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
     }
-
     dispatch({
       type: LOGIN_FAIL,
     });
   }
 };
 
-// Logout
-export const logout = () => ({ type: LOGOUT });
+export const logout = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
+  dispatch({
+    type: LOGOUT,
+  });
+};
