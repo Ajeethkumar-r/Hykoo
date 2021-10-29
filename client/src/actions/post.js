d5b64130d772b994e454e617f147002a974d5430
iamajeeth <rajeethkumarind01@gmail.com>
2021-10-29 13:48:13 +0530

    feat: Delete post  need to fix

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import api from '../utils/api';
 import { setAlert } from './alertActions';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
 
 // eslint-disable-next-line
 export const getPosts = () => async (dispatch) => {
@@ -32,6 +32,7 @@ export const addLike = (id) => async (dispatch) => {
     });
   }
 };
+
 export const removeLike = (id) => async (dispatch) => {
   try {
     const res = await api.put(`/posts/unlike/${id}`);
@@ -39,6 +40,22 @@ export const removeLike = (id) => async (dispatch) => {
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.delete(`/posts/${id}`);
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+    dispatch(setAlert('Post Removed', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
